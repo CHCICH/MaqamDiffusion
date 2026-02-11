6557a54700d7bcf942a959f9472b96cbc739c84f
CHICH <charbelh162@gmail.com>
2026-02-11 19:08:39 +0200

    last autoencoder

---
+++ src/models/Autencoder.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+#this is the definiton of the Autoencoder block this could be used in both the encoder and the decoder 
+
+class AutoEncoderBlock(nn.Module):
+    def __init__(self, in_c, out_c,encode=False):
+
+        super(AutoEncoderBlock, self).__init__()
+
+        self.encode = encode
+        self.in_channel = in_c
+        self.out_channel = out_c
+        self.conv = nn.Conv2d(in_c, out_c, kernel_size=3, stride=2)
+        if encode:
+            self.maxPooling = nn.MaxPool2d(kernel_size=2,stride=2 )
+            self.up_conv = None
+        else:
+            self.maxPooling = None
+            self.up_conv = nn.ConvTranspose2d(out_c, out_c,4,2,1)
+    
+
+        self.relu = nn.ReLU()
+        self.bnorm = nn.BatchNorm2d(out_c)
+
+    def forward(self,x):
+        x_p = self.conv(x)
+        x_p = self.relu(self.bnorm(x_p))
+        if self.encode:
+            x_p = self.maxPooling(x_p)
+        else:
+            x_p = self.up_conv(x_p)
+        return x_p
+    
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            AutoEncoderBlock(1, 32, encode=True),
+            AutoEncoderBlock(32, 64, encode=True),
+            AutoEncoderBlock(64, 128, encode=True)
+        )   
+
+        self.decoder = nn.Sequential(
+            AutoEncoderBlock(encode=False,in_c=128,out_c=64),
+            AutoEncoderBlock(encode=False,in_c=64,out_c=32),
+            AutoEncoderBlock(encode=False,in_c=32,out_c=1),
+        )
+
+    def forward(self,x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+    def encode_latent(self,x):
+        x = self.encoder(x)
+        return x
+    
+    def decode_latent(self,x):
+        x = self.decoder(x)
+        return x
+    
+
