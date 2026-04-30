5d9a15291f01853629e59cde6d4f9bd620abe61a
CHCICH <charbelh162@gmail.com>
2026-04-30 07:07:20 -0400

    sending to hpc

--- src/testing/autoencoder_training.py
+++ src/testing/autoencoder_training.py
@@ -9,10 +9,8 @@ from models.Data_Load import (
 )
 from models.Autencoder import AutoEncoder, Classifier
 
-# Dummy image-like data: batch=4, channels=1, height=256, width=256
 
 device = setup_torch()
-# how to print the directory I am in
 print(f"Current directory: {os.getcwd()}")
 
 data_tensor = torch.load("json_data/dataset_updated.pt", map_location=device)
@@ -35,7 +33,6 @@ dataLoader_test = DataLoader_AutoEncoder_Classifier(
 
 
 def test_model(model, dataLoader, Loss_fn, optimizer, num_epochs):
-
     model.eval()
     total_mse = 0.0
     total_mae = 0.0
@@ -58,7 +55,6 @@ def test_model(model, dataLoader, Loss_fn, optimizer, num_epochs):
 
 
 def train(epoch, lr_rate, dataLoader, Loss_fn, optimizer):
-
     latent_model = AutoEncoder()
     latent_model.to(device)
 
@@ -70,6 +66,7 @@ def train(epoch, lr_rate, dataLoader, Loss_fn, optimizer):
         Loss_fn = torch.nn.BCEWithLogitsLoss()
     else:
         raise ValueError("Invalid Loss Function")
+
     if optimizer == "Adam":
         optimizer = torch.optim.Adam(latent_model.parameters(), lr=lr_rate)
     elif optimizer == "AdamW":
@@ -82,7 +79,10 @@ def train(epoch, lr_rate, dataLoader, Loss_fn, optimizer):
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.955)
     num_epochs = epoch
     epochList = []
+
     for epoch in range(num_epochs):
+        latent_model.train()
+
         for batch in dataLoader:
             images, labels = batch
             images = images.to(device)
@@ -93,21 +93,20 @@ def train(epoch, lr_rate, dataLoader, Loss_fn, optimizer):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
         scheduler.step()
         epochList.append(loss.item())
         print(f"lr = {lr_rate} Loss {epoch + 1}/{num_epochs} Loss is L ={loss.item()} ")
 
     return latent_model, Loss_fn, optimizer, num_epochs, epochList
 
-    # this is the first attempt to be able to do the classifications of Maqam in the latent space without contrastive learning methods
-
 
 testing = False
 already_trained = True
 train_autoencoder = True
+
 if train_autoencoder:
     if testing:
-        # LR_rate = [0.00001, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.5]
         LR_rate = [1e-3]
         epoch = 300
 
@@ -117,9 +116,10 @@ if train_autoencoder:
                 epoch, lr_rate, dataLoader, Loss_fn="MSE", optimizer="Adam"
             )
             List_of_all_hyperparams.append(epochList)
+
         with open("data.json", "w") as f:
             json.dump(List_of_all_hyperparams, f)
-        # here we are going to start the training for the Classifier in other words the bottle neck classifier
+
     else:
         if not already_trained:
             LR_rate = 1e-3
@@ -130,7 +130,8 @@ if train_autoencoder:
             torch.save(latent_model.state_dict(), "model_weights.pth")
         else:
             latent_model = AutoEncoder()
-            latent_model.load_state_dict(torch.load("model_weights.pth"))
+            latent_model.load_state_dict(torch.load("model_weights.pth", map_location=device))
+            latent_model.to(device)
 
 
 def converter_class_idx(class_idx, in_mapping=False):
@@ -144,6 +145,7 @@ def converter_class_idx(class_idx, in_mapping=False):
         "saba": 5,
         "segah": 2,
     }
+
     class_mapping = {
         1: "bayat",
         7: "hijaz",
@@ -163,45 +165,67 @@ def converter_class_idx(class_idx, in_mapping=False):
 
 def convert_label_list(list_label):
     new_list = []
+
     for label in list_label:
         new_elem = int(converter_class_idx(label, True))
         y = [0, 0, 0, 0, 0, 0, 0, 0]
         y[new_elem] = 1
         new_list.append(y)
+
     return torch.tensor(new_list, dtype=torch.float32)
 
 
 def train_classifier(epoch, lr_rate, dataLoader, Loss_fn, optimizer, input_size):
     latent_classifier = Classifier(input_size, 8)
     latent_classifier.to(device)
+
     latent_autoencoder = AutoEncoder()
-    latent_autoencoder.load_state_dict(torch.load("model_weights.pth"))
-    if optimizer == "Adam":
-        optimizer = torch.optim.Adam(latent_autoencoder.parameters(), lr=lr_rate)
+    latent_autoencoder.load_state_dict(torch.load("model_weights.pth", map_location=device))
     latent_autoencoder.to(device)
+    latent_autoencoder.eval()
+
+    if optimizer == "Adam":
+        optimizer = torch.optim.Adam(latent_classifier.parameters(), lr=lr_rate)
+    elif optimizer == "AdamW":
+        optimizer = torch.optim.AdamW(latent_classifier.parameters(), lr=lr_rate)
+    elif optimizer == "SGD":
+        optimizer = torch.optim.SGD(latent_classifier.parameters(), lr=lr_rate)
+    else:
+        raise ValueError("Invalid Optimizer")
 
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.955)
     num_epochs = epoch
 
     loss_value = []
+
     for epoch in range(num_epochs):
+        latent_classifier.train()
+
         for batch in dataLoader:
             images, labels = batch
             images = images.to(device)
-            labels = convert_label_list(labels)
-            encoded_images = latent_autoencoder.encode_latent(images)
-            encoded_images = torch.flatten(encoded_images, start_dim=1)
+            labels = convert_label_list(labels).to(device)
+
+            with torch.no_grad():
+                encoded_images = latent_autoencoder.encode_latent(images)
+                encoded_images = torch.flatten(encoded_images, start_dim=1)
+
             output = latent_classifier(encoded_images)
             loss = Loss_fn(output, labels)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
         scheduler.step()
+
         print(
             f"Cross_Entropy lr = {lr_rate} Loss {epoch + 1}/{num_epochs} Loss is L ={loss.item()} "
         )
+
         loss_value.append(loss.item())
-    torch.save(latent_classifier.state_dict(), "autoencoder_weight.pth")
+
+    torch.save(latent_classifier.state_dict(), "classifier_weight.pth")
 
     return loss_value
 
@@ -279,54 +303,65 @@ def train_contrasitve_model(epoch_count, dataLoader, weight_L):
 LR_rate = [0.00001, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.5]
 
 
-#    t = train_classifier(
-#        300, 0.01, dataLoader, torch.nn.CrossEntropyLoss(), "Adam", input_size=2048
-#    )
-#    with open("loss_graph_new.json", "w") as f:
-#        json.dump(t, f)
+t = train_classifier(
+    300, 0.01, dataLoader, torch.nn.CrossEntropyLoss(), "Adam", input_size=2048
+)
+with open("loss_graph_new.json", "w") as f:
+    json.dump(t, f)
 
 
 latent_autoencoder = AutoEncoder()
-latent_autoencoder.load_state_dict(torch.load("model_weights.pth"))
+latent_autoencoder.load_state_dict(torch.load("model_weights.pth", map_location=device))
+latent_autoencoder.to(device)
+latent_autoencoder.eval()
+
 classifier = Classifier(2048, 8)
-classifier.load_state_dict(torch.load("autoencoder_weight.pth"))
+classifier.load_state_dict(torch.load("classifier_weight.pth", map_location=device))
+classifier.to(device)
+classifier.eval()
+
 correct_ones = 0
 total = 0
+
 with torch.inference_mode():
     for batch in dataLoader:
         images, labels = batch
         images = images.to(device)
         labels = convert_label_list(labels).to(device)
 
-        output_autoencoder = images
-
         encoded_images = latent_autoencoder.encode_latent(images)
         encoded_images = torch.flatten(encoded_images, start_dim=1)
 
         output_classifier = classifier(encoded_images)
+
         predicted_label = torch.argmax(output_classifier, dim=1)
         real_label = torch.argmax(labels, dim=1)
+
         correct_ones += (predicted_label == real_label).sum().item()
         total += labels.size(0)
-accuarcy = correct_ones / total
 
+accuarcy = correct_ones / total
 
 print(f"Correct: {correct_ones}/{total}")
 print(f"Accuracy: {accuarcy * 100:.2f}%")
 
-# loss_graph = []
 
+# loss_graph = []
+#
 # for lr in LR_rate:
-#    input_size = 16384
-#    t = train_classifier(
-#        200, lr, dataLoader, torch.nn.CrossEntropyLoss(), "Adam", input_size
-#    )
-#    loss_graph.append(t)
+#     input_size = 16384
+#     t = train_classifier(
+#         200, lr, dataLoader, torch.nn.CrossEntropyLoss(), "Adam", input_size
+#     )
+#     loss_graph.append(t)
+#
 # with open("loss_graph.json", "w") as f:
-#    json.dump(loss_graph, f)
-#    total_data = []
-#    for i in range(5):
-#        data_got = train_contrasitve_model(300, dataLoader, 0.1 * i)
-#        total_data.append(data_got)
-#    with open("loss_contrastive.json", "w") as f:
-#        json.dump(total_data, f)
+#     json.dump(loss_graph, f)
+#
+# total_data = []
+# for i in range(5):
+#     data_got = train_contrasitve_model(300, dataLoader, 0.1 * i)
+#     total_data.append(data_got)
+#
+# with open("loss_contrastive.json", "w") as f:
+#     json.dump(total_data, f)
