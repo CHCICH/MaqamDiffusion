e836c421c65452e14e7b0d5fee6e4ff193dd25a0
CHCICH <charbelh162@gmail.com>
2026-05-20 12:19:39 -0400

    sending to hpc

--- src/testing/data_plotting/data_autoencoder_hyperParams.py
+++ src/testing/data_plotting/data_autoencoder_hyperParams.py
@@ -4,21 +4,15 @@ import json
 
 data_collected = None
 
-with open("best_epoch.json", "r") as f:
+with open("real_contrastive_now.json", "r") as f:
     data_collected = json.load(f)
 
 
-train_acc, test_acc, train_loss, test_loss = data_collected
+train_acc, test_acc = data_collected
 
 epoch_space = [i + 1 for i in range(len(train_acc))]
 
 plt.plot(epoch_space, train_acc)
 
 plt.plot(epoch_space, test_acc)
-plt.show()
-
-plt.plot(epoch_space, train_loss)
-
-plt.plot(epoch_space, test_loss)
-
 plt.show()
