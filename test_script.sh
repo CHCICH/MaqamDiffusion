2ec1bc1ffe4ca3239cb6cc67d2b6f60488ba1cfc
CHCICH <charbelh162@gmail.com>
2026-06-03 01:35:49 -0400

    sending to hpc

--- test_script.sh
+++ test_script.sh
@@ -3,16 +3,13 @@
 #SBATCH --job-name=trainingautoencoder
 #SBATCH --account=cbe05
 
-# Fix 1: Change partition from normal to gpu
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=12000
-
-# Fix 2: Correct the GPU resource name to the v100d16q queue
-#SBATCH --gres=gpu:v100d16q:1
-#SBATCH --time=0-00:20:00
+#SBATCH --mem=32000
+#SBATCH --gres=gpu:v100d32q:1
+#SBATCH --time=0-01:30:00
 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=cbe05@mail.aub.edu
@@ -21,5 +18,7 @@ echo "Program will start executing in a bit"
 module purge 
 module load python/ai-4
 
+
 source .diffusion/bin/activate
 python results/jmw.py
+
