7b72b7f2a52c8b4495895d51ec523ae6f925d168
CHCICH <charbelh162@gmail.com>
2026-06-03 01:15:07 -0400

    sending to hpc

---
+++ test_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=trainingautoencoder
+#SBATCH --account=cbe05
+
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32000
+#SBATCH --gres=gpu:v100d32q:1
+#SBATCH --time=0-01:30:00
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cbe05@mail.aub.edu
+
+echo "Program will start executing in a bit"
+module purge 
+module load python/ai-4
+
+
+source .diffusion/bin/activate
+python results/jmw.py
+
+
+
+
